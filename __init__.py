07506ea90b1cd1258e2682d54f950af1cdb8ce9c
Sean Marvi Oliver Genabe <seanmarviolivergenabe@gmail.com>
2019-09-16 03:50:05 +0800

    anki-times

--- __init__.py
+++ __init__.py
@@ -1,115 +1,191 @@
-from aqt import mw
-from aqt.utils import showInfo, chooseList, showText
-from aqt.qt import *
-from itertools import chain
 from anki.utils import stripHTMLMedia, intTime, ids2str
-import time
+from aqt import mw
+from aqt.qt import *
+from aqt.utils import showInfo, chooseList, showText, getSaveFile, getFile
+from itertools import chain
+import json
+import re
 import sys
+import time
 
 MAX_RETRIES = 6
 
-def _copyScheduling(deckFrom, deckTo):
-    now = intTime()
-    logs = []
-    cids = mw.col.decks.cids(deckTo["id"], children=False)
-    copiedN = 0
-    updates = []
-    
+
+def exportDeck():
+    deckId = chooseDeck(prompt="Choose deck to export scheduling from")
+    if deckId == 0:
+        return
+    cids = mw.col.decks.cids(deckId, children=False)
+    cards = {}
+
     for cid in cids:
         card = mw.col.getCard(cid)
         note = mw.col.getNote(card.nid)
-        sfld = stripHTMLMedia(note.fields[note.col.models.sortIdx(note._model)])
-        sourceCids = mw.col.db.list(
-            "select distinct(c.id) from cards c, notes n where c.nid=n.id and c.did=? and n.sfld=?",
-            deckFrom["id"],
-            sfld
-        )
-        
-        # If there are no source cards, skip
-        if not sourceCids:
-            continue
-        if len(sourceCids) > 1:
-            logs.append("Multiple source cards not supported. Matched field={0}".format(sfld))
-            continue
-        sourceCid = sourceCids[0]
-        sourceCard = mw.col.getCard(sourceCid)
+        sfld = stripHTMLMedia(
+            note.fields[note.col.models.sortIdx(note._model)])
         # Skip new cards
-        if sourceCard.queue == 0:
+        if card.queue == 0:
             continue
+        if sfld in cards:
+            showText("Card with duplicated field found; aborting.")
+            return
 
-        logs.append("Matched card {0}".format(sfld))
-        
-        updates.append(dict(
-            cid=cid,
-            due=sourceCard.due,
-            queue=sourceCard.queue,
-            ivl=sourceCard.ivl,
-            factor=sourceCard.factor,
-            left=sourceCard.left,
-            type=sourceCard.type,
-            now=now,
-            usn=mw.col.usn()
+        revlogKeys = ["id", "ease", "ivl", "lastIvl", "factor", "time", "type"]
+        revlogsArray = mw.col.db.all(
+            "select " +
+            ", ".join(revlogKeys) +
+            " from revlog " +
+            "where cid = ?",
+            cid
+        )
+        revlogsDict = list(map(
+            lambda row:
+            {revlogKeys[i]: row[i]
+                for i in range(0, len(revlogKeys))}, revlogsArray
         ))
 
-        def copyRevlog(offset):
-            mw.col.db.execute(
-                "insert into revlog "
-                "select r.id + :offset, :newcid, :usn, r.ease, r.ivl, r.lastIvl, r.factor, r.time, r.type "
-                "from revlog as r "
-                "where cid=:oldcid",
-                offset=offset,
-                oldcid=sourceCid,
-                newcid=cid,
-                usn=mw.col.usn()
-            )
-        for i in range(0, MAX_RETRIES + 1):
-            try:
-                copyRevlog(2 << i)
-                break
-            except:
-                if i == MAX_RETRIES:
-                    raise
+        cards[sfld] = dict(
+            due=card.due,
+            queue=card.queue,
+            ivl=card.ivl,
+            factor=card.factor,
+            left=card.left,
+            type=card.type,
+            lapses=card.lapses,
+            reps=card.reps,
+            flags=card.flags,
+            revlogs=revlogsDict
+        )
 
-        copiedN += 1
-
-    #logs.append("updates {0}".format(updates))
-
-    mw.col.db.executemany(
-        "update cards set "
-        "due=:due, mod=:now, usn=:usn, queue=:queue, "
-        "ivl=:ivl, factor=:factor, left=:left, type=:type "
-        "where id=:cid",
-        updates
+    file = getSaveFile(
+        mw,
+        "Export scheduling info to file",
+        "anki-times",
+        "JSON",
+        ".json",
+        re.sub('[\\\\/?<>:*|"^]', '_',
+               mw.col.decks.name(deckId)) + "-scheduling"
     )
-    
-    logs.append("Copied {0} cards".format(copiedN))
-
-    showText("\n".join(logs), title="Copy scheduling log")
-    mw.reset()
-
-def copyScheduling():
-    decks = mw.col.decks.all()
-
-    deckFromIndex = chooseList("Choose deck to copy from", chain(map(lambda deck: deck['name'], decks), ["Cancel"]))
-    if deckFromIndex == len(decks):
+    if not file:
         return
-    otherDecks = decks.copy()
-    otherDecks.pop(deckFromIndex)
-    deckToIndex = chooseList("Choose deck to copy to", chain(map(lambda deck: deck['name'], otherDecks), ["Cancel"]))
-    if deckToIndex == len(otherDecks):
-        return
-    
-    deckFrom = decks[deckFromIndex]
-    deckTo = otherDecks[deckToIndex]
 
-    mw.checkpoint("Copy scheduling")
+    output = {
+        "meta": {
+            "crt": mw.col.crt
+        },
+        "cards": cards
+    }
+
+    with open(file, "w") as f:
+        json.dump(output, f, ensure_ascii=False)
+
+
+def importDeck():
+    mw.checkpoint("Import scheduling info")
     try:
-        _copyScheduling(deckFrom, deckTo)
+        _importDeck()
     except:
         mw.col.db.rollback()
         raise
 
-action = QAction("Copy Scheduling", mw)
-action.triggered.connect(copyScheduling)
 
-mw.form.menuTools.addAction(action)
+def _importDeck():
+    logs = []
+    now = intTime()
+    importedN = 0
+
+    deckId = chooseDeck(prompt="Choose deck to import scheduling into")
+    if deckId == 0:
+        return
+    file = getFile(mw, "", None, filter="*.json", key="anki-times")
+    if not file:
+        return
+
+    data = {}
+    with open(file, "r") as f:
+        data = json.load(f)
+
+    crt = data["meta"]["crt"]
+    cards = data["cards"]
+
+    for key in cards:
+        src = cards[key]
+        destCids = mw.col.db.list(
+            "select distinct(c.id) from cards as c, notes as n "
+            "where c.nid=n.id and c.did=? and n.sfld=?",
+            deckId,
+            key
+        )
+
+        # If there are no destination cards, skip
+        if not destCids:
+            continue
+        if len(destCids) > 1:
+            logs.append(
+                f"Multiple destination cards not supported. Matched field={key}")
+            continue
+        logs.append(f"Matched card {key}")
+
+        destCid, = destCids
+
+        mw.col.db.execute(
+            "update cards set "
+            "due=:due, mod=:now, usn=:usn, queue=:queue, lapses=:lapses, "
+            "reps=:reps, flags=:flags, "
+            "ivl=:ivl, factor=:factor, left=:left, type=:type "
+            "where id=:cid",
+            cid=destCid,
+            now=now,
+            usn=mw.col.usn(),
+            **src
+        )
+
+        for i in range(0, MAX_RETRIES + 1):
+            try:
+                if i == 0:
+                    importRevlogs(0, destCid, src["revlogs"])
+                else:
+                    importRevlogs(1 << (i - 1), destCid, src["revlogs"])
+                break
+            except:
+                if i == MAX_RETRIES:
+                    raise
+        importedN += 1
+
+    logs.append(f"Copied {importedN} cards")
+
+    showText("\n".join(logs), title="Import scheduling info log")
+    mw.reset()
+
+
+def importRevlogs(offset, cid, revlogs):
+    mw.col.db.executemany(
+        "insert into revlog " +
+        "values(:id + :offset, :cid, :usn,"
+        ":ease, :ivl, :lastIvl, :factor, :time, :type)",
+        map(
+            lambda revlog: dict(offset=offset, cid=cid,
+                                usn=mw.col.usn(), **revlog),
+            revlogs
+        )
+    )
+
+
+def chooseDeck(prompt="Choose deck"):
+    "Shows a deck selector and returns the deck ID. If cancelled, return 0."
+    choices = sorted(mw.col.decks.allNames())
+    chosenIndex = chooseList(prompt, ["Cancel"] + choices)
+    if chosenIndex == 0:
+        return 0
+
+    return mw.col.decks.id(choices[chosenIndex - 1])
+
+
+def createAction(label, fn, target=mw.form.menuTools):
+    action = QAction(label, mw)
+    action.triggered.connect(fn)
+    target.addAction(action)
+
+
+createAction("Export scheduling info...", exportDeck)
+createAction("Import scheduling info...", importDeck)
--- readme.md
+++ readme.md
@@ -1,14 +1,5 @@
-# Anki Copy Scheduling add-on
+# Anki Import/Export Scheduling add-on
 
-This add-on copies scheduling info from one deck to another.
+This add-on imports and exports scheduling info from one deck to another.
 
 Use case: Migrating from a deck to another similar deck.
-
-* Cards are compared by their sort field. Only cards with a matching sort field are processed.
-  * Cards from the old deck that are "new" are ignored.
-  * Cards that don't match a card in the old deck are ignored.
-  * The scenario where there are multiple cards representing a note _in the old deck_ is not yet supported.
-* All scheduling info are overwritten.
-* All `revlog`s of the cards from the old deck are copied.
-  * Make sure to run the process only once; the revlogs are copied each run.
-* Undo support
